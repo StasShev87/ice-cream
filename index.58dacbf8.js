49fcc674f5d3fc32b77cd8c1ae5ad9d14c407055
dvolodko <41117606+dvolodko@users.noreply.github.com>
2022-09-20 06:49:46 +0000

    Deploying to gh-pages from @ dvolodko/group-project4-icecream@0cf9a9719b9ccb89810162034353959efa2333df 🚀

---
+++ index.58dacbf8.js
@@ -0,0 +1,2 @@
+!function(){let o=$(".back-to-home");$(window).on("scroll",(()=>{$(this).scrollTop()>=50?o.fadeIn():o.fadeOut()})),o.on("click",(o=>{o.preventDefault(),$("html").animate({scrollTop:0},1e3)}))}();
+//# sourceMappingURL=index.58dacbf8.js.map
---
+++ index.6b524ec0.js
@@ -0,0 +1,2 @@
+!function(){var o=this,n=$(".back-to-home");$(window).on("scroll",(function(){$(o).scrollTop()>=50?n.fadeIn():n.fadeOut()})),n.on("click",(function(o){o.preventDefault(),$("html").animate({scrollTop:0},1e3)}))}();
+//# sourceMappingURL=index.6b524ec0.js.map
